fb265036a72fa672ff7e496ae2f801fc4c076522
Ali Yekda Hare <aliyekdahare10@gmail.com>
2021-06-22 16:10:52 +0300

    KDV Tutarı Hesaplayan Program eklendi.

---
+++ KDVTutarıHesaplayanProgram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class KDVTutarıHesaplayanProgram {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ÜCRET TUTARINI GİRİNİZ:");
+        float a = sc.nextFloat();
+        if(a>0  && a<=1000){
+            System.out.println("KDV'siz Fiyat: "+a);
+            System.out.println("KDV'li Fiyat: "+(((a*18)/100)+a));
+            System.out.println("KDV Tutarı: "+((a*18)/100));
+        }
+        else if (a>1000){
+            System.out.println("KDV'siz Fiyat: "+a);
+            System.out.println("KDV'li Fiyat: "+(((a*8)/100)+a));
+            System.out.println("KDV Tutarı: "+((a*8)/100));
+        }
+
+    }
+
+} 
