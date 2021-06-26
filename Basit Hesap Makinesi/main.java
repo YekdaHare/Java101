64444f87999ec5eb70b8859e3149cd3347899cb3
Ali Yekda Hare <aliyekdahare10@gmail.com>
2021-06-26 08:26:37 +0300

    Manav Kasa Programı eklendi.

---
+++ Basit Hesap Makinesi/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class main {    
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        System.out.print("İLK SAYIYI GİRİNİZ: ");
+        a = input.nextInt();
+        System.out.print("İKİNCİ SAYIYI GİRİNİZ: ");
+        b = input.nextInt();
+        System.out.print("LÜTFEN BİR İŞLEM SEÇİNİZ.");
+        System.out.println("\n1-TOPLAMA\n2-ÇIKARMA\n3-ÇARPMA\n4-BÖLME");
+        int c = input.nextInt();
+        switch(c){
+            case 1:
+                System.out.print("TOPLAMA İŞLEMİ SEÇTİNİZ: "+ (a+b));
+                break;
+            case 2:
+                System.out.print("ÇIKARMA İŞLEMİ SEÇTİNİZ: "+ (a-b));
+                break;
+            case 3:
+                System.out.print("ÇARPMA İŞLEMİ SEÇTİNİZ: "+ (a*b));
+                break;
+            case 4: 
+                System.out.println("BÖLME İŞLEMİ SEÇTİNİZ: "+ (a/b));
+                break;
+            default:
+                System.out.println("LÜTFEN GEÇERLİ BİR DEĞER GİRİNİZ...");
+        }
+    }
+    
+}
