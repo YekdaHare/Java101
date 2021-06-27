59a82cc00b8d8fa93ed12133323b06dd4a74be90
YekdaHare <81968919+YekdaHare@users.noreply.github.com>
2021-06-27 21:30:14 +0300

    Dizideki Elemanları Sıralama programı eklendi.

---
+++ DizidekiElemanlarıSıralama.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class JavaProject1 {    
+    public static void main(String[] args) {
+        //Scanner sınıfı eklendi.
+         Scanner input = new Scanner(System.in);
+         int temporary;
+         int piece;
+         System.out.print("LÜTFEN GİRİLECEK SAYI ADEDİNİ BELİRTİNİZ: "); piece=input.nextInt();
+         //array.
+        int [] array = new int[10];
+        //for döngüleri eklendi.
+        for(int i = 0; i < piece; i++)
+        {
+            System.out.print("LÜTFEN DİZİNİZİN " + (i+1) + " . ELEMANINI GİRİNİZ: ");
+            array[i] = input.nextInt();
+        }
+        System.out.println("DİZİNİZİN KÜÇÜKTEN BÜYÜĞE SIRALANMIŞ HALİ: ");
+        for(int i = 0; i < piece-1; i++)
+        {
+            for(int j = i+1; j < piece; j++)
+            {
+                if(array[j] < array[i]) {
+                    temporary = array[i];
+                    array[i] = array[j];
+                    array[j] = temporary;
+                }
+            }
+        }
+        for(int i = 0; i < piece; i++)
+        {
+            System.out.print("-" + array[i]);
+        }
+    }
+}
