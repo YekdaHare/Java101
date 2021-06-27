54da383f97ded6bc972b8ed95c6d51b83372e09a
YekdaHare <81968919+YekdaHare@users.noreply.github.com>
2021-06-27 21:31:51 +0300

    Update DizidekiElemanlarıSıralama.java

--- DizidekiElemanlarıSıralama.java
+++ DizidekiElemanlarıSıralama.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JavaProject1 {    
+public class main {    
     public static void main(String[] args) {
         //Scanner sınıfı eklendi.
          Scanner input = new Scanner(System.in);
