613c58e59ca8d0e5b37ee11b03d3612a26f02690
YekdaHare <81968919+YekdaHare@users.noreply.github.com>
2021-06-27 21:32:45 +0300

    Update MatrisTranspozunuBulma.java

--- MatrisTranspozunuBulma.java
+++ MatrisTranspozunuBulma.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JavaProject1 {    
+public class main {    
     public static void main(String[] args) {
         //Scanner Sınıfı eklendi.
         Scanner input = new Scanner(System.in);
