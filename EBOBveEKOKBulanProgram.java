5744ee2761a64cd26201113cae79de1519239050
YekdaHare <81968919+YekdaHare@users.noreply.github.com>
2021-06-27 21:33:14 +0300

    Update EBOBveEKOKBulanProgram.java

--- EBOBveEKOKBulanProgram.java
+++ EBOBveEKOKBulanProgram.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JavaProject1 {    
+public class main {    
     public static void main(String[] args) {
         // Scanner Sınıfı Tanımlandı.
          Scanner input = new Scanner(System.in);
